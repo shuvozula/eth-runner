09b6b3bcf00e0a9135c6f872ffe3160b8744c30b
Shuvo Saha <shuvozula@gmail.com>
2018-06-09 18:23:24 -0700

    converting tabs to spaces

--- miner_watchdog.sh
+++ miner_watchdog.sh
@@ -11,22 +11,22 @@ sleep $startIn;
 echo "Watchdog started!"
 while :
 do
-	for device_wattage in $watchCmd
-	do
-		if [ "$device_wattage" -lt "$limit" ]
-		then
-			echo "`date`: Current power usage is $currentPowerUsage < $limit killing miner" | tee -a watchdogLog.log
-		
-			sh $my_dir/stop.sh
-			sleep 20;
-		
-			SLEEP_FOR=15 # minutes
-			SLEEP_FOR_SECS=$(($SLEEP_FOR*60))
-			WAKEUP_IN=$(date +%H:%M -d "+$SLEEP_FOR_SECS seconds")
-			echo "Going to sleep for $SLEEP_FOR minutes..., waking up at $WAKEUP_IN."
-			sh $my_dir/switchoff_until.sh $WAKEUP_IN
-			exit 0
-		fi
-		sleep 10;
-	done
+    for device_wattage in $watchCmd
+    do
+        if [ "$device_wattage" -lt "$limit" ]
+        then
+            echo "`date`: Current power usage is $currentPowerUsage < $limit killing miner" | tee -a watchdogLog.log
+        
+            sh $my_dir/stop.sh
+            sleep 20;
+        
+            SLEEP_FOR=15 # minutes
+            SLEEP_FOR_SECS=$(($SLEEP_FOR*60))
+            WAKEUP_IN=$(date +%H:%M -d "+$SLEEP_FOR_SECS seconds")
+            echo "Going to sleep for $SLEEP_FOR minutes..., waking up at $WAKEUP_IN."
+            sh $my_dir/switchoff_until.sh $WAKEUP_IN
+            exit 0
+        fi
+        sleep 10;
+    done
 done
