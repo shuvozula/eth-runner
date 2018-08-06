7710943c72bbef011b16aeb3bebe2b6972b7a6e3
Shuvo Saha <shuvozula@gmail.com>
2018-08-06 00:28:38 -0700

    Added some sleep before starting data collection. Also forgot to create DB before collection, so fixed that...

--- metrics/start_metrics.sh
+++ metrics/start_metrics.sh
@@ -9,16 +9,21 @@ else
   echo "Creating container...."
   docker run \
     --ulimit nofile=66000:66000 \
-		-d \
-		--name $METRICS_SERVER_NAME \
-		-p 3003:3003 \
-		-p 3004:8888 \
-		-p 8086:8086 \
-		-p 22022:22 \
-		-p 8125:8125/udp \
-		samuelebistoletti/docker-statsd-influxdb-grafana:latest
+    -d \
+    --name $METRICS_SERVER_NAME \
+    -p 3003:3003 \
+    -p 3004:8888 \
+    -p 8086:8086 \
+    -p 22022:22 \
+    -p 8125:8125/udp \
+    samuelebistoletti/docker-statsd-influxdb-grafana:latest
+
+  echo "Creating DB: $METRICS_DB"
+  curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE $METRICS_DB"
 fi
 
+echo "Sleeping for 10 secs for InfluxDB to startup..."
+sleep 10
 
 upload_wattage_metrics() {
   watchPower="/usr/bin/nvidia-smi -q -d POWER | grep \"Power Draw\" | sed 's/[^0-9,.]*//g' | cut -d . -f 1"
@@ -32,6 +37,7 @@ upload_wattage_metrics() {
 }
 
 start_collecting_metrics() {
+  echo "Starting data collection..."
   while :
   do
     upload_wattage_metrics
