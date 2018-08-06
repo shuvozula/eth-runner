cb617de124f6f5cf97098e60ce1e6d5d90d95989
Shuvo Saha <shuvozula@gmail.com>
2018-08-06 01:45:19 -0700

    upload heat metrics

--- metrics/start_metrics.sh
+++ metrics/start_metrics.sh
@@ -27,11 +27,22 @@ fi
 
 upload_wattage_metrics() {
   watchPower="/usr/bin/nvidia-smi -q -d POWER | grep \"Power Draw\" | sed 's/[^0-9,.]*//g' | cut -d . -f 1"
-  currTime=$(date +%s)
   i=0
+  host="http://localhost:8086/write?db=${METRICS_DB}"
   for device_wattage in $(eval ${watchPower}); do
-    curl -i -XPOST 'http://localhost:8086/write?db=${METRICS_DB}' \
-        --data-binary 'gpu_${i}_power,host=minar value=${device_wattage} ${currTime}'
+    data_binary="gpu${i}heat,host=minar,gpu=${i} power=${device_wattage}"
+    echo $data_binary | curl -i -XPOST $host --data-binary @-
+    ((i++))
+  done
+}
+
+upload_heat_metrics() {
+  watchHeat="/usr/bin/nvidia-smi -q -d TEMPERATURE | grep \"GPU Current Temp\" | sed 's/[^0-9,.]*//g' | cut -d . -f 1"
+  i=0
+  host="http://localhost:8086/write?db=${METRICS_DB}"
+  for device_heatage in $(eval ${watchHeat}); do
+    data_binary="gpu${i}heat,host=minar,gpu=${i} heat=${device_heatage}"
+    echo $data_binary | curl -i -XPOST $host --data-binary @-
     ((i++))
   done
 }
@@ -41,6 +52,7 @@ start_collecting_metrics() {
   while :
   do
     upload_wattage_metrics
+    upload_heat_metrics
     sleep 1
   done
 }
