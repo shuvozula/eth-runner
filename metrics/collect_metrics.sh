d146b7bff02184fae87f4ec4e0e1ffd3343530c2
Shuvo Saha <shuvozula@gmail.com>
2018-08-07 19:12:30 -0700

    updated to use nanoseconds as InfluxDB expects it, also added fan metrics

--- metrics/collect_metrics.sh
+++ metrics/collect_metrics.sh
@@ -2,6 +2,7 @@
 
 METRICS_DB=ethmetrics
 HOST="http://localhost:8086/write?db=${METRICS_DB}"
+GPUS=$(sudo DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -c :0 -q gpus)
 
 upload_wattage_metrics() {
   watchPower="/usr/bin/nvidia-smi -q -d POWER | grep \"Power Draw\" | sed 's/[^0-9,.]*//g' | cut -d . -f 1"
@@ -23,13 +24,22 @@ upload_heat_metrics() {
   done
 }
 
+upload_fan_metrics() {
+  for gpu_index in $(echo $GPUS | grep -o "\[[0-9]*\]" | grep -o "[0-9]*"); do
+    watchFanCmd="/usr/bin/nvidia-smi -i ${gpu_index} -q | grep \"Fan Speed\" | sed 's/[^0-9,.]*//g' | cut -d . -f 1"    
+    data_binary="gpu${i}fanspeed,host=minar,gpu=${i} fanspeed=${watchFanCmd} $1"
+    echo $data_binary | curl -s -i -XPOST $HOST --data-binary @- 2>&1 >/dev/null
+  done
+}
+
 start_collecting_metrics() {
   echo "Starting data collection..."
   while :
   do
-    date=$(date +%s)
+    date=$(date +%s%N)  # nanoseconds since epoch
     upload_wattage_metrics $date
     upload_heat_metrics $date
+    upload_fan_metrics $date
     sleep 1
   done
 }
