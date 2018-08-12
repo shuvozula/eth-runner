f66f5c7b2b57640dca5f0a923b38d719006b74d8
Shuvo Saha <shuvozula@gmail.com>
2018-08-12 15:38:22 -0700

    fixed the stop metrics script

--- metrics/stop_metrics.sh
+++ metrics/stop_metrics.sh
@@ -6,7 +6,7 @@ METRICS_SERVER_NAME=eth-metrics-influx
 sudo kill -9 $(cat /var/log/metrics_collection.pid)
 
 # stop the metrics containers
-if [[ $(docker ps -a | grep $METRICS_SERVER_NAME) ]]; then
-	echo "Stopping metrics server..."
-	docker stop $METRICS_SERVER_NAME
+if [ "$(docker ps -a | grep $METRICS_SERVER_NAME)" ]; then
+  echo "Stopping metrics server..."
+  docker stop $METRICS_SERVER_NAME
 fi
