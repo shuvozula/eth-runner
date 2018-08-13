79f76256b8141c04e3ee8d976c1ea56852fa5945
Shuvo <shuvozula@gmail.com>
2018-08-12 18:12:38 -0700

    fixed the stop script to stop metrics services as well

--- stop.sh
+++ stop.sh
@@ -5,7 +5,7 @@ my_dir="$(dirname $0)"
 sh $my_dir/stop_watchdog.sh
 sh $my_dir/stop_miners.sh
 
-if [ "$1" -eq "--metrics" ]; then
+if [[ $1 == --metrics ]]; then
   echo 'Killing metrics containers and data collection...'
   sudo sh $my_dir/metrics/stop_metrics.sh
 fi
