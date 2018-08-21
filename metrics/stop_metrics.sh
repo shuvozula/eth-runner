4fadb48b4d2560b70725e989a2837aafdc5a999c
Shuvo Saha <shuvozula@gmail.com>
2018-08-20 21:20:36 -0700

    converting the metrics collection for Nvidia GPUs from shell to pythonb. Uses the pynvml package, which comes with the default NVIDIA drivers

--- metrics/stop_metrics.sh
+++ metrics/stop_metrics.sh
@@ -5,7 +5,7 @@ METRICS_SERVER_NAME=eth-metrics-influx
 # stop the metrics collection script
 echo "Stopping metrics collectors..."
 sudo kill -9 $(cat /var/log/nvidia_metrics_collector.pid)
-sudo kill -9 $(cat /var/log/amd_cpu_metrics_collectors.pid)
+sudo kill -9 $(cat /var/log/amd_cpu_metrics_collector.pid)
 
 # stop the metrics containers
 if [ "$(docker ps -a | grep $METRICS_SERVER_NAME)" ]; then
